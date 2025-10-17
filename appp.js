259793c24792f298568d2d4165915f47f58f8dfc
ameermoavia3git config --global user.email amavia115@gmail.comgit config --listgit config --global user.name ameermoavia3 <amavia115@gmail.com>
2025-10-17 14:24:10 +0500

    first commit

---
+++ appp.js
@@ -0,0 +1,45 @@
+var display =document.getElementById("display")
+console.log(display);
+
+function press(vol){
+    display.value += vol;
+}
+
+function cleandisplay(){
+    display.value = ""
+}
+
+function delchar(){
+    display.value = display.value.slice(0, -1)
+}
+
+function calculate() {
+    var text = display.value;
+    var opt = "+-*/%"
+
+    if (text === " ") {
+        display.value = " ";
+        alert("Please add on  number and operator")
+        return
+    }else if (opt.includes(text[0]) || opt.includes(text[text.length -1])){
+            display.value = "Error";
+            return
+    }
+
+    var erorFound = false;
+    
+    for (let i = 0; i < text.length -1; i++) {
+        if (opt.includes(text[0]) || opt.includes(text[text.length -1])) {
+            erorFound = true;
+            break;
+        }
+        
+    }
+
+    if (erorFound) {
+        display.value = "Error";
+    }else{
+        display.value = eval(text)
+    }
+}
+
