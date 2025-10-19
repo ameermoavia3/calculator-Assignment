95f0edfd103ac955a6708e443872cf4d17ee1074
ameermoavia3git config --global user.email amavia115@gmail.comgit config --listgit config --global user.name ameermoavia3 <amavia115@gmail.com>
2025-10-20 00:53:22 +0500

    6 th commit

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var display =document.getElementById("display")
 console.log(display);
 
 var resultShown = false;
+var errorShown = false;
 
 function press(vol){
 
-    if (resultShown) {
+     if (errorShown) {
+        display.value = "";
+        errorShown = false; 
+    }
+       if (resultShown) {
         display.value = "";
         resultShown = false;
     }
@@ -30,6 +35,7 @@ function calculate() {
         return
     }else if (opt.includes(text[0]) || opt.includes(text[text.length -1])){
             display.value = "Error";
+            errorShown = true;
             return
     }
  
@@ -45,10 +51,14 @@ function calculate() {
 
     if (errorFound) {
         display.value = "Error";
+        errorShown = true;
+        return;
     }else{
         display.value = eval(text)
+    
     }
 
      resultShown = true;
+     errorShown = false;
 }
 
