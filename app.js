1655eb8718ad47c92204a44b38cb01bad8d9aa92
Ameer Moavia <159649056+ameermoavia3@users.noreply.github.com>
2025-10-21 23:42:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,79 @@
-var display =document.getElementById("display")
+var display =document.getElementById("display");
 console.log(display);
 
 var resultShown = false;
 var errorShown = false;
 
-function press(vol){
 
-     if (errorShown) {
-        display.value = "";
-        errorShown = false; 
-    }
-       if (resultShown) {
-        display.value = "";
+function press(vol) {
+    
+    if (display.value === "Error") {
+        display.value = vol;
+        errorShown = false;
         resultShown = false;
+        return;
+    }
+
+    
+    if (resultShown) {
+        var ops = "+-*/%";
+        if (ops.includes(vol)) {
+            
+            display.value += vol;
+        } else {
+
+            display.value = vol;
+        }
+        resultShown = false;
+        return;
+    }
+
+    if (errorShown) {
+        display.value = vol;
+        errorShown = false;
+        return;
     }
     display.value += vol;
 }
 
+
+
+
+
 function cleandisplay(){
-    display.value = ""
+    display.value = "";
 }
 
 function delchar(){
-    display.value = display.value.slice(0, -1)
+    display.value = display.value.slice(0, -1);
 }
 
 function calculate() {
     var text = display.value;
     var opt = "+-*/%"
+    var opt1 = ".+-*/%"
 
     if (text === "") {
         display.value = "";
-        alert("Please add on  number and operator")
-        return
-    }else if (opt.includes(text[0]) || opt.includes(text[text.length -1])){
-            display.value = "Error";
-            errorShown = true;
-            return
-    }
+        alert("Please add on  number and operator");
+        return;
+
+   } else if (opt.includes(text[text.length - 1])) {
+    
+    display.value = "Error";
+    errorShown = true;
+    return;
+} else if (opt.includes(text[0]) && text[0] !== '-' && text[0] !== '+') {
+    
+    display.value = "Error";
+    errorShown = true;
+    return;
+}
  
     var errorFound = false;
     
     for (var i = 0; i < text.length -1; i++) {
-        if (opt.includes(text[i]) && opt.includes(text[i + 1])) {
+        if (opt1.includes(text[i]) && opt1.includes(text[i + 1])) {
             errorFound = true;
             break;
         }
@@ -54,11 +85,20 @@ function calculate() {
         errorShown = true;
         return;
     }else{
-        display.value = eval(text)
+        try {
+    display.value = eval(text);
+} catch (e) {
+    display.value = "Error";
+    errorShown = true;
+    return;
+}
+
     
     }
 
      resultShown = true;
      errorShown = false;
 }
+
+
 
